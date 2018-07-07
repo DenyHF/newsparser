6783ac975610a4b96a0385070a36158f61d1c019
Dmitry Khaperets <dmitry.khaperets@mgid.com>
2018-07-07 14:50:24 +0300

    [Core] Refactoring

--- src/Consumer/Queue/Project/Channel/ParseConsumer.php
+++ src/Consumer/Queue/Project/Channel/ParseConsumer.php
@@ -3,9 +3,9 @@
 namespace App\Consumer\Queue\Project\Channel;
 
 use App\Infrastructure;
-use App\Domain\Entity\ProjectInterface;
 use App\Domain\ValueObject\Consumer\MessageInterface;
 use App\Domain\Consumer\DependencyInjection\ProjectDependencyInterface;
+use App\Domain\Service\CommandBus\Entity\Project\Article\CreateCommandInterface;
 
 /**
  * @property ProjectDependencyInterface $dependency
@@ -18,16 +18,19 @@ class ParseConsumer extends Infrastructure\Consumer\Consumer
     public const QUEUE_NAME = 'app:queue:project:channel:parse';
 
     /**
-     * @var array
+     * @var CreateCommandInterface
      */
-    protected $projects;
+    protected $createArticleCommand;
 
     /**
      * @param ProjectDependencyInterface $dependency
+     * @param CreateCommandInterface     $createArticleCommand
      */
-    public function __construct(ProjectDependencyInterface $dependency)
+    public function __construct(ProjectDependencyInterface $dependency, CreateCommandInterface $createArticleCommand)
     {
         parent::__construct($dependency);
+
+        $this->createArticleCommand = $createArticleCommand;
     }
 
     /**
@@ -36,35 +39,15 @@ class ParseConsumer extends Infrastructure\Consumer\Consumer
     protected function onMessage(MessageInterface $message): void
     {
         $payload = $message->getPayload();
-        $project = $this->getProject($payload['project']);
+        $project = $this->dependency->getProject($payload['project']);
         $channelParser = new Infrastructure\Service\Api\Rss\ChannelParser();
 
         foreach ($project->getChannels() as $channel) {
             $this->dependency->getLogger()->debug('Parse Channel', ['url' => $channel->getUrl()]);
 
             foreach ($channelParser->getArticles($channel) as $article) {
-                $this->dependency->getCreateArticleCommand()->handle($article);
+                $this->createArticleCommand->handle($article);
             }
         }
-    }
-
-    /**
-     * @param string $id
-     *
-     * @return ProjectInterface
-     *
-     * @throws \InvalidArgumentException
-     */
-    protected function getProject(string $id): ProjectInterface
-    {
-        if (false === isset($this->projects[$id])) {
-            $this->projects[$id] = $this->dependency->getProjectRepository()->find($id);
-        }
-
-        if (null === $this->projects[$id]) {
-            throw new \InvalidArgumentException(sprintf('Project with ID "%s" not found.', $id));
-        }
-
-        return $this->projects[$id];
     }
 }
--- src/Infrastructure/Consumer/DependencyInjection/ProjectDependency.php
+++ src/Infrastructure/Consumer/DependencyInjection/ProjectDependency.php
@@ -3,14 +3,19 @@
 namespace App\Infrastructure\Consumer\DependencyInjection;
 
 use Psr\Log\LoggerInterface;
+use App\Domain\Entity\ProjectInterface;
 use App\Domain\Consumer\QueueInterface;
 use App\Domain\Repository\ProjectRepositoryInterface;
 use App\Domain\Service\Api\Rss\ChannelParserInterface;
-use App\Domain\Service\CommandBus\Entity\Project\Article;
 use App\Domain\Consumer\DependencyInjection\ProjectDependencyInterface;
 
 class ProjectDependency extends ConsumerDependency implements ProjectDependencyInterface
 {
+    /**
+     * @var array
+     */
+    protected $projects;
+
     /**
      * @var ChannelParserInterface
      */
@@ -22,30 +27,22 @@ class ProjectDependency extends ConsumerDependency implements ProjectDependencyI
     protected $projectRepository;
 
     /**
-     * @var Article\CreateCommandInterface
-     */
-    protected $createArticleCommand;
-
-    /**
-     * @param QueueInterface                 $queue
-     * @param LoggerInterface                $logger
-     * @param ChannelParserInterface         $channelParser
-     * @param ProjectRepositoryInterface     $projectRepository
-     * @param Article\CreateCommandInterface $createArticleCommand
+     * @param QueueInterface             $queue
+     * @param LoggerInterface            $logger
+     * @param ChannelParserInterface     $channelParser
+     * @param ProjectRepositoryInterface $projectRepository
      */
     public function __construct(
         QueueInterface $queue,
         LoggerInterface $logger,
         ChannelParserInterface $channelParser,
-        ProjectRepositoryInterface $projectRepository,
-        Article\CreateCommandInterface $createArticleCommand
+        ProjectRepositoryInterface $projectRepository
     )
     {
         parent::__construct($queue, $logger);
 
         $this->channelParser = $channelParser;
         $this->projectRepository = $projectRepository;
-        $this->createArticleCommand = $createArticleCommand;
     }
 
     /**
@@ -65,10 +62,22 @@ class ProjectDependency extends ConsumerDependency implements ProjectDependencyI
     }
 
     /**
-     * {@inheritdoc}
+     * @param string $id
+     *
+     * @return ProjectInterface
+     *
+     * @throws \InvalidArgumentException
      */
-    public function getCreateArticleCommand(): Article\CreateCommandInterface
+    public function getProject(string $id): ProjectInterface
     {
-        return $this->createArticleCommand;
+        if (false === isset($this->projects[$id])) {
+            $this->projects[$id] = $this->projectRepository->find($id);
+        }
+
+        if (null === $this->projects[$id]) {
+            throw new \InvalidArgumentException(sprintf('Project with ID "%s" not found.', $id));
+        }
+
+        return $this->projects[$id];
     }
 }
